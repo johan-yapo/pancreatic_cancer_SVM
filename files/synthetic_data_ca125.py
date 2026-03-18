7a557da81a70a030be88f10076c5e2dd3e07cfe9
J0chin <64190045+J0chin@users.noreply.github.com>
2026-03-18 16:57:51 -0500

    completed synthetic data generation

--- files/synthetic_data_ca125.py
+++ files/synthetic_data_ca125.py
@@ -12,7 +12,21 @@ RESULTS_PATH = os.path.join(os.path.dirname(curr_dir), 'Results')
 real_data = pd.read_csv(os.path.join(DATA_PATH, "machine_learning_data_template_ca125 serum.csv"))
 
 TARGET = "Class_Multimodal"  
+print(TARGET)
 
 # Define metadata
+
 metadata = SingleTableMetadata()
 metadata.detect_from_dataframe(real_data)
+
+# Initialize and train CTGAN synthesizer
+synth = CTGANSynthesizer(metadata, epochs=300, batch_size=512)
+synth.fit(real_data)
+
+n_minority = 1000
+n_majority = 1000
+
+# Generate synthetic minority class data
+syn_min = synth.sample_from_conditions(
+    conditions=pd.DataFrame([{TARGET: 1}]*n_minority)
+)
